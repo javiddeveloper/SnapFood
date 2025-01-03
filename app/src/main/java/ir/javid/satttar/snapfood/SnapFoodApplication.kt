151c11c64c93732690d0d2cf74506ace2df5e2c7
Javid sattar <nice.jdsr.0917@gmail.com>
2025-01-03 23:45:53 +0330

    Add Application class

---
+++ app/src/main/java/ir/javid/satttar/snapfood/SnapFoodApplication.kt
@@ -0,0 +1,13 @@
+package ir.javid.satttar.snapfood
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author  : Javid
+ * @summary : SnapFoodApplication
+ */
+
+@HiltAndroidApp
+class SnapFoodApplication:Application() {
+}
